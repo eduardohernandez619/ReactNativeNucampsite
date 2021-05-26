122f20ba34e148abb659e48a8809327a688e36dd
Ed Hernandez <help.uds@gmail.com>
2021-05-26 06:53:31 -0700

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import {Card} from 'react-native-elements';
 import {CAMPSITES} from '../shared/campsites';
+import * as Animatable from 'react-native-animatable';
 
 
 function RenderItem({item}) {
@@ -32,15 +33,17 @@ class Contact extends Component {
     render() {
         return (
             <ScrollView>
-                <Card
-                    title="Contact Information"
-                    wrapperStyle={{margin:20}}>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card
+                        title="Contact Information"
+                        wrapperStyle={{margin:20}}>
                         <Text>1 Nucamp Way</Text>
                         <Text>Seattle, WA 98001</Text>
                         <Text style={{marginBottom: 10}}>U.S.A</Text>
                         <Text>Phone: 1-206-555-1234</Text>
                         <Text>Email: campsites@nucamp.co</Text>
                     </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }
